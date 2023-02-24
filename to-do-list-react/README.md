0e572294911e2f15d5a97b041bb6730fb0d5a2f6
Arif ESKİCİ <aaema4@gmail.com>
2023-02-25 01:58:11 +0300

    ToDoList

---
+++ to-do-list-react/README.md
@@ -0,0 +1,21 @@
+# To Do List with React
+
+- First, we run the `npx create-react-app` command in the directory where we will work.
+
+- Then we run our project with `npm start`
+
+## About the App
+
+- The purpose of this application; React was made to learn and reinforce.
+
+- After adding our tasks to the To do list :
+
+  - `All : `
+    ![1](./images/1.png)
+  - `Active : `
+    ![2](./images/2.png)
+  - `Completed : `
+    ![3](./images/3.png)
+  - `some fine details : `
+    ![4](./images/4.png)
+    ![5](./images/5.png)
---
+++ to-do-list-react/src/components/Contacts/index.js
@@ -0,0 +1,20 @@
+import {useState} from 'react';
+import Header from './header/Header';
+import List from './list/List';
+import Footer from './footer/Footer';
+
+function Contacts() {
+
+    const [contacts, setContacts] = useState([]);
+    const [situation, setSituation]=useState("All")
+    
+  return (
+    <div>
+        <Header setContacts={setContacts} contacts={contacts}/>   
+        <List contacts={contacts} setContacts={setContacts} situation={situation}/>
+        <Footer contacts={contacts} setContacts={setContacts} situation={situation} setSituation={setSituation}/>
+    </div>
+  )
+}
+
+export default Contacts;
